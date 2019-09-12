871e24b6be0f251a366c5391d35326011e6e0795
Solomon Matthews <smatthews@ricoh.co.nz>
2019-09-12 22:27:37 +0000

    Default to only providng normalized fields

--- lib/process.py
+++ lib/process.py
@@ -10,14 +10,14 @@ def addFields(df, name_dict):
     for new, orig in name_dict.items():
         df[new] = df[orig]
 
-def processFile(s3_url, show_fields=False):
+def processFile(s3_url, show_fields=False, keep_orig=False):
     p = readFromS3(s3_url)
+    orig_fields = p.columns
     names = p.columns.tolist()
     
     toner = findFields(names, '.*Toner.Bottle.%s.Remaining.Toner.(?!previous).*', 'Toner.%s')
     addFields(p, toner)
     toner_names = list(toner.keys())
-    toner_raw = list(toner.values())
     
     pages = findFields(names, '.*Pages.Current.Toner.%s.(?!previous).*', 'Pages.Toner.%s')
     addFields(p, pages)
@@ -27,6 +27,7 @@ def processFile(s3_url, show_fields=False):
 
     cov = findFields(names, '.*Pixel.Coverage.Accumulation.Coverage.%s.*', 'Coverage.%s')
     addFields(p, cov)
+    cov_names = list(cov.keys())
     
     dev_replacement = findFields(
         names,
@@ -63,17 +64,18 @@ def processFile(s3_url, show_fields=False):
     times = ['RetrievedDate', 'RetrievedDateTime']
 
     # Add fields for 
-    to_add = list(toner_names + times)
+    to_add = list(toner_names + cov_names + times)
     delta_fields = [x+'.delta' for x in to_add]
     time_deltas = [x+'.delta' for x in times]
     replacement_fields = [x+'.replaced' for x in toner_names]
 
-    print(f"Adding deltas and replacement fields for {s3_url}")
+    print(f"Adding deltas, rates and replacement fields for {s3_url}")
     addDeltas(p, to_add)
     # Coerce values to timedeltas / NaT
     for d in time_deltas:
         p[d] = pd.to_timedelta(p[d])
     p['RetrievedDateTime.delta.days']=p['RetrievedDateTime.delta'] / datetime.timedelta(days=1)
+    addRates(p, to_add)
     addReplacements(p, toner_names)
     
     print("Adding toner bottle indices")
@@ -92,12 +94,25 @@ def processFile(s3_url, show_fields=False):
     print("Project coverage for toner bottles and calculate ratios vs. estimated coverage")
     for color in colors_norm:
         projectCoverage(p, color)
-        
+
     models = p.Model.unique()
     print("Add toner usage ratios")
     for color in colors_norm:
         for m in models: 
             addTonerRatio(p, color, m)
+
+    if not keep_orig:
+        print("Dropping original fields")
+        to_use = set(p.columns).difference(orig_fields)
+        # Keep these regardless
+        to_use.update([
+            'Serial',
+            'Model',
+            'RetrievedDate',
+            'RetrievedDateTime',
+            'FileDate',
+        ])
+        p = p[to_use]
     
     return(p)
 
@@ -131,13 +146,7 @@ def selectTonerStats(df):
     return res
 
 
-models = ['E15', 'E16', 'E17', 'E18', 'E19']
-#models = ['E18', 'E19']
-#models = ['E26']
-#models = ['G70', 'G71', 'G72', 'G73', 'G74', 'G75']
-
-
-def doProcessing(path, summary_rows_only=True):
+def doProcessing(path, summary_rows_only=False):
     try:
         res = processFile(f"s3://{in_bucket_name}/{path}")
         if summary_rows_only:
@@ -148,51 +157,11 @@ def doProcessing(path, summary_rows_only=True):
         res=None
     return res
 
-#to_use = [x for x in cs if x[0] in models]
-#to_use = cs
 
-
-def buildDataset(to_use, num_procs=int(np.ceil(multiprocessing.cpu_count() / 2))):
+def buildDataset(to_use, num_procs=int(np.ceil(multiprocessing.cpu_count() / 2)), f=doProcessing):
     with multiprocessing.Pool(num_procs) as pool:
         res_parts = pool.map(doProcessing, to_use, chunksize=1)
     res_parts = [x for x in res_parts if x is not None]
     print("Combining result parts")
     res = pd.concat(res_parts)
     return res
-
-
-
-#sers = res.loc[res['Projected.Coverage.Y'] < 10000, 'Serial'].unique()
-#sers
-
-#toner = findFields(
-#    names,
-#    [#'.*Current.Toner.%s.(?!previous).*',
-#     '.*Toner.Bottle.%s.Remaining.Toner.(?!previous).*'],
-#    'Toner.%s'
-#)
-#prev_pages = findFields(names, '.*Pages.Current.Toner.%s.previous.*', 'Pages.Previous.Toner.%s')
-#pages = findFields(names, '.*Pages.Current.Toner.%s.(?!previous).*', 'Pages.Toner.%s')
-#coverage = findFields(names, '.*Pixel.Coverage.Accumulation.Coverage.%s.*', 'Coverage.%s')
-##At least one model has two apparently identical counters for this (same name, different number). Use the first one if so.
-#developer_rotation = findFields(
-#    names,
-#    ['.*(?<!Previous.Unit).PM.Counter.Rotation.Developer.%s.*',
-#     '.*Drive.Distance.Counter.%s_Developer.*'],
-#    'Developer.Rotation.%s',
-#    take_first=True)
-#
-#all_fields = [
-#    toner,
-#    prev_pages,
-#    pages,
-#    coverage,
-#    developer_rotation,
-#]
-#
-#for d in all_fields:
-#    for k, v in d.items():
-#        print(f"{k}: {v}")
-        
-#fields = reduce(lambda x,y: x.update(y) or x, all_fields, {})
-
