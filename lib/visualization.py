3aa0ee9e001b157ae3dcab1511c847c094908fb5
Solomon Matthews <smatthews@ricoh.co.nz>
2019-10-05 01:04:09 +0000

    Plot AtRemote internal data latency

---
+++ lib/visualization.py
@@ -0,0 +1,40 @@
+import datetime
+
+import numpy as np
+import plotly
+import plotly.graph_objs as go
+
+
+def plotAtRemoteLatency(df, max_days=7, days_history=None):
+    if days_history:
+        start = datetime.datetime.today() - datetime.timedelta(days=days_history)
+        df = df[df.FileDate > start.date()]
+
+    dates = []
+    latencies = []
+    counts = []
+
+    for d, df1 in df.groupby('FileDate'):
+        #print(d)
+        ls = df1.FileDate - df1.RetrievedDate
+        ls = ls.apply(lambda x: x.days)
+        freqs = np.histogram(
+            ls.clip(upper=max_days),
+            range=(0, max_days),
+            bins=max_days+1,
+        )[0]
+        dates += [d]*(max_days+1)
+        latencies += range(max_days+1)
+        counts += list(freqs)
+    fig = go.Figure(data=go.Heatmap(
+            x=dates,
+            y=latencies,
+            z=counts,
+            colorscale='Viridis',
+            #colorscale='Electric'
+    ))
+    fig.update_layout(
+        title='RNZ F@lcon Data Internal Latency',
+        #xaxis_nticks=36
+    )
+    fig.show()
