ae90a283ad4ceded91d66d1ec8779c529bd5a71c
Solomon Matthews <smatthews@ricoh.co.nz>
2021-05-24 00:53:28 +0000

    More memory per process for dataset build

--- lib/process.py
+++ lib/process.py
@@ -345,7 +345,7 @@ process_nz_sers = None
 @timed
 def buildDataset(to_use, kwargs={}, num_procs=None, f=doProcessing):
     if num_procs is None:
-        num_proces = int(np.ceil(multiprocessing.cpu_count() / 2)),
+        num_proces = int(np.ceil(multiprocessing.cpu_count() / 4)),
     global process_nz_sers
     ser_df = pd.read_csv('s3://ricoh-prediction-misc/mif/current/customer.csv')
     process_nz_sers = ser_df.SerialNo
